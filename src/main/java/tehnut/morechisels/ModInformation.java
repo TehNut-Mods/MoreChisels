35e44f6c78868508539bb919349f4682df2196a0
TehNut <nick_ignoffo@yahoo.com>
2015-08-28 18:29:58 -0700

    The convention invitation had the wrong address
    #26

--- src/main/java/tehnut/morechisels/ModInformation.java
+++ src/main/java/tehnut/morechisels/ModInformation.java
@@ -8,7 +8,7 @@ public class ModInformation {
     public static final String CHANNEL = "MoreChisels";
     public static final String DEPEND = "required-after:chisel;after:SSTOW";
     public static final String VERSION = "@VERSION@";
-    public static final String CLIENTPROXY = "tehnut.morechisels.proxies.ClientProxy";
-    public static final String COMMONPROXY = "tehnut.morechisels.proxies.CommonProxy";
+    public static final String CLIENTPROXY = "tehnut.morechisels.proxy.ClientProxy";
+    public static final String COMMONPROXY = "tehnut.morechisels.proxy.CommonProxy";
     public static final String GUIFACTORY = "tehnut.morechisels.client.gui.ConfigGuiFactory";
 }
