85c547d99c67763fc71905a847238300768c03f2
RCXcrafter <rcxcrafter@gmail.com>
2016-04-23 15:05:50 +0200

    renamed that to ExtraCells
    apparently that didn't fit in one commit :S

--- src/main/java/tehnut/morechisels/compat/CompatibilityExtracells.java
+++ src/main/java/tehnut/morechisels/compat/CompatibilityExtracells.java
@@ -5,14 +5,13 @@ import tehnut.morechisels.registry.RecipeRegistry;
 import tehnut.morechisels.registry.ItemRegistry;
 import tehnut.morechisels.item.ChiselType;
 import tehnut.morechisels.item.ItemChiselBase;
-import tehnut.morechisels.item.chisel.ItemChiselVoidmetal;
 import tehnut.morechisels.util.LogHelper;
 
 import static tehnut.morechisels.ConfigHandler.*;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 
-public class CompatibilityExtracells {
+public class CompatibilityExtraCells {
 
     public static Item chiselWalrus;
 
