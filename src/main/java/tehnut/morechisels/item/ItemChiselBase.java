eb3182bfb0f84262b48ed2281b251e0d9a26ffdc
RCXcrafter <rcxcrafter@gmail.com>
2016-04-24 11:46:32 +0200

    Chisels are now held like tools in 3rd person by default

--- src/main/java/tehnut/morechisels/item/ItemChiselBase.java
+++ src/main/java/tehnut/morechisels/item/ItemChiselBase.java
@@ -47,6 +47,13 @@ public class ItemChiselBase extends Item implements IChiselItem {
         return this.type;
     }
 
+    /**
+     * @return - Returns weather the chisel should be rendered like a tool in third persson.
+     */
+	public boolean isFull3D() {
+		return true;
+	}
+
     // Start Chisel API
 
     @Override
